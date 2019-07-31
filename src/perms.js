7f1f9a09f13a80ecf1628f674873b57582f3999b
Glitch (notdlockly) <none>
2019-07-31 22:25:04 +0000

    🐩🏐 Checkpoint ./src/perms.js:712571/36 ./src/consts.js:712571/36

--- src/perms.js
+++ src/perms.js
@@ -5,6 +5,6 @@ const server = require('../server');
 
 module.exports.isAdmin = function (user) {
   var memberInOurGuild = consts.dlocklyGuild().member(user);
-  if (memberInOurGuild && memberInOurGuild.roles.has('601489434084507649')) return true;
+  if (memberInOurGuild && memberInOurGuild.roles.has('520016204862062603')) return true;
   else return false;
 }
