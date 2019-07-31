02e53443ece0800f8df7d2a0427f4177d0a0acfd
Glitch (notdlockly) <none>
2019-07-31 01:52:59 +0000

    🛳📺 Checkpoint ./src/perms.js:712571/72 ./www/css/dark.css:712571/146 ./src/consts.js:712571/38

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@
 const server = require('../server');
 
 module.exports.dlocklyGuild = function () {
-  return server.bot.guilds.get('519997113648676879');//
+  return server.bot.guilds.get('591692042304880815');//
 }
 
 module.exports.votesChannel = function () {
