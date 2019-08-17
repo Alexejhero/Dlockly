e959864b8aa4a342aa09a1b93e494d9eb7d019d7
Glitch (dlockly) <none>
2019-08-17 09:09:48 +0000

    Glitch Update

--- config-old.js
+++
@@ -1,9 +0,0 @@
-var chn, msg;
-
-function Respond_to_bot_message(chn, msg) {
-  await chn.send(msg);}
-
-
-
-module.exports.message = async message => { try {  Respond_to_bot_message(message.channel, message);
-} catch (e) { require("../../src/errors").onerror("610877150181982210", e); } }
